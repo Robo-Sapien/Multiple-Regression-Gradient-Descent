343025928d7273789f5e30df3923ef20e50872a0
nik-12 <f20150283@hyderabad.bits-pilani.ac.in>
2018-11-14 23:17:43 +0530

    changes

--- Regression/regression.py
+++ Regression/regression.py
@@ -7,10 +7,24 @@ np.random.seed(1)
 
 class Regression:
 	"""
-
+	class for performing regression
 	"""
 	def __init__(self,addr,split_ratio,validation_ratio):
 		"""
+		Class constructor
+		
+		params:
+		@addr: address of data file
+		@split_ratio: fraction of data to be used for training the model
+		@validation_ration: fraction of data to be used for validation
+
+		attributes:
+		@fileaddr: address of the file containing the data
+		@data: dataframe containing values of features and value of target var
+		@training_size: size of training data
+		@validation_size: size of validation data
+		@features: value of features for each entry in the data
+		@target: value of target variables for each entry in the data
 
 		"""
 		self.file_addr = addr
@@ -28,7 +42,9 @@ class Regression:
 
 	def feature_scaling(self):
 		"""
-
+		
+		function to scale the values of features so that they all acquire values in the same range 
+		
 		"""
 		mean_value = self.features.mean(axis=0)
 		std = self.features.std(axis=0)
@@ -50,6 +66,8 @@ class Regression:
 	def normal_equation(self):
 		"""
 
+		Method to use normal equation method for getting value of parameters in regression expression
+		
 		"""
 		#print(self.x_test)
 
@@ -63,9 +81,9 @@ class Regression:
 
 		#print(parameters)
 
-		output_val = np.matmul(parameters,self.x_train.transpose())
+		output_val = np.matmul(parameters,self.x_test.transpose())
 
-		error = self.error_estimation(output_val,2)
+		error = self.error_estimation(output_val,1)
 
 		#print("error == > " + str(error))
 		return error
@@ -73,6 +91,13 @@ class Regression:
 
 	def gradient_descent_without_regularization(self,iterations,learning_rate):
 		"""
+		
+		Method to estimate the value of parameters in linear regression without
+		imposing any restriction on the value of parameters. 
+		
+		params:
+		@iterations: to impose restriction on the number of iterations
+		@learning_rate: learning rate to be used in the stochastic gradient descent
 
 		"""
 		parameters = np.random.randn(5,)
@@ -83,6 +108,7 @@ class Regression:
 		prev_error = 0.0
 
 		while 1>0:
+		# for i in range(0,iterations):
 			feature = self.x_train
 			prediction = np.matmul(initial_parameters,feature.transpose())
 			prediction = prediction.transpose()
@@ -95,8 +121,8 @@ class Regression:
 
 			# print(parameters)
 
-			output_val = np.matmul(parameters,self.x_train.transpose())					
-			curr_error = self.error_estimation(output_val,2)
+			output_val = np.matmul(parameters,self.x_test.transpose())					
+			curr_error = self.error_estimation(output_val,1)
 			initial_parameters = parameters
 
 			if abs(curr_error - prev_error) < 10**(-3) or abs(curr_error - prev_error) > 10**(3):
@@ -115,8 +141,18 @@ class Regression:
 		return prev_error
 
 
-	def gradient_descent_with_rigged_regularization(self,iterations,learning_rate,reg_coeff):
+	def gradient_descent_with_ridge_regularization(self,iterations,learning_rate,reg_coeff):
 		"""
+
+		Method to estimate the value of parameters in linear regression without
+		imposing any restriction on the value of parameters.
+		
+		Ridge regression: imposing restriction on the value of square of parameters
+
+		params:
+		@iterations: to impose restriction on the number of iterations
+		@learning_rate: learning rate to be used in the stochastic gradient descent
+		@reg_coeff: regularization coeff used in ridge regression
 
 		"""
 		parameters = np.random.randn(5,)
@@ -126,6 +162,7 @@ class Regression:
 		prev_error = 0.0
 
 		while 1>0:
+		# for i in range(0,iterations):	
 			feature = self.x_train
 			prediction = np.matmul(initial_parameters,feature.transpose())
 			prediction = prediction.transpose()
@@ -135,8 +172,8 @@ class Regression:
 				
 			initial_parameters = parameters
 			
-			output_val = np.matmul(parameters,self.x_train.transpose())						
-			curr_error = self.error_estimation(output_val,2)
+			output_val = np.matmul(parameters,self.x_test.transpose())						
+			curr_error = self.error_estimation(output_val,1)
 
 			if abs(curr_error - prev_error) < 10**(-4) or abs(curr_error - prev_error) > 10**(3):
 				prev_error = curr_error
@@ -152,6 +189,13 @@ class Regression:
 		
 		lasso regression which imposes linear constraints on the values of parameters
 		
+		Lasso regression: imposing restriction on the absolute value of parameters
+
+		params:
+		@iterations: to impose restriction on the number of iterations
+		@learning_rate: learning rate to be used in the stochastic gradient descent
+		@reg_coeff: regularization coeff used in ridge regression
+
 		"""
 		parameters = np.random.randn(5,)
 		num = self.x_train.shape[0]
@@ -169,8 +213,8 @@ class Regression:
 				
 			initial_parameters = parameters
 			
-			output_val = np.matmul(parameters,self.x_train.transpose())
-			curr_error = self.error_estimation(output_val,2)
+			output_val = np.matmul(parameters,self.x_test.transpose())
+			curr_error = self.error_estimation(output_val,1)
 
 			if abs(curr_error - prev_error) < 10**(-4) or abs(curr_error - prev_error) > 10**(3):
 				prev_error = curr_error
@@ -183,6 +227,15 @@ class Regression:
 
 	def error_estimation(self,output_val,flag):
 		"""
+		
+		estimating the test,train or validation error
+
+		params:
+		@output_val: estimated value of target variable
+		@flag: variable to indicate whether test, train or validation error is being calculated
+				flag=1 => test error
+				flag=2 => training error
+				flag=0 => validation error 
 
 		"""
 		if flag == 1:
@@ -215,13 +268,21 @@ class Regression:
 		return error
 
 	def plots_no_regularization(self,X,Y,x_name,y_name):
-		"""
+		"""	
+		
+		function to generate the plots
+
+		params:
+		@X: x axis parameters
+		@Y: y axis parameters
+		@x_name: name of the x axis
+		@y_name: name of the y axis
 
 		"""
 		fig = plt.figure()
 		fig.suptitle("reg_coeff vs validation_error",fontsize=20)
 			
-		print(Y[0])
+		#print(Y[0])
 
 		for i in range(0,len(X)): 	
 			x = X[i]
@@ -242,6 +303,15 @@ class Regression:
 
 	def plots_with_regularization(self,X,Y,reg_coeff,x_name,y_name):
 		"""
+		
+		function to generate the plots
+
+		params:
+		@X: x axis parameters
+		@Y: y axis parameters
+		@reg_coeff: regularization coeff
+		@x_name: name of the x axis
+		@y_name: name of the y axis
 
 		"""
 		fig = plt.figure()
@@ -271,6 +341,9 @@ if __name__ == '__main__':
 	error = reg.normal_equation()
 	print("normal_equation error =>" + str(error))
 	#reg.normal_equation()
+	
+	# to generate the plot of validation error vs learning rate without regularization
+
 	# X = []
 	# Y = []
 	# Z = []
@@ -295,6 +368,8 @@ if __name__ == '__main__':
 
 	# reg.plots_no_regularization(X,Y,Z,"iterations","error")
 
+
+	# to generate the plot of validation error vs reg_coeff for ridge regression and lasso regression 
 
 	# learning_rate = 0.01
 	# reg_coeff = 0.0
@@ -353,8 +428,8 @@ if __name__ == '__main__':
 	error = reg.gradient_descent_without_regularization(0,0.0815)
 	print("No regularization error =>" + str(error))
 
-	error = reg.gradient_descent_with_rigged_regularization(0,0.0815,0.3)
-	print("Rigged regression error =>" + str(error))	
+	error = reg.gradient_descent_with_ridge_regularization(0,0.0815,0.3)
+	print("Ridge regression error =>" + str(error))	
 
 	error = reg.gradient_descent_with_lasso_regularization(0,0.0815,0.8)
 	print("Lasso regression error =>" + str(error))		
@@ -402,5 +477,6 @@ if __name__ == '__main__':
 
 	# 	learning_rate = learning_rate*5
 
-	#reg.gradient_descent_with_rigged_regularization(10000,0.001,1)
+	#reg.gradient_descent_with_ridge_regularization(10000,0.001,1)
 	#reg.gradient_descent_with_lasso_regularization(10000,0.001,1)
+	
